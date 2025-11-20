28767e27a3513e4b6ac60544d15311be7f8b24c3
Jonathan Priebe <jonathan.priebe@gmx.de>
2025-11-20 03:13:47 +0000

    Fix submodule paths

--- admin_panel/dwc_admin/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class DwcAdminConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'dwc_admin'
--- admin_panel/dwc_api/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class DwcApiConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'dwc_api'
